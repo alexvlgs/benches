9263b0f41f11821ecedc52d1ca9a7df1fc5eadcc
Alexandre Villegas <bello.alex@hotmail.com>
2021-02-23 11:10:08 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#  lower_case_user = User.create!(first_name: "Peter", last_name: "Griffin", email: "griffin@gmail.com", password: "monkeys123")  
+#  lower_case_user = User.create!(first_name: "Peter", last_name: "Griffin", email: "griffin@gmail.com", password: "monkeys123")
 #  lower_case_bench = Bench.create!( name: "buttcomfort", location: "buttland", user: lower_case_user)
 
 User.destroy_all
@@ -14,7 +14,7 @@ Booking.destroy_all
 
 puts "creating 10 users"
 
-10.times do 
+10.times do
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: '1234hello')
 end
 puts "done"
@@ -30,3 +30,5 @@ puts "creating 10 bookings"
   Booking.create(user: User.all.sample, bench: Bench.all.sample)
 end
 puts "done"
+
+puts "hi"
