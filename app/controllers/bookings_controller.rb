629d5595672d85849924fb3575b8ff4b0953f3e1
Susanna Pezzini <susanna.pezzini@gmail.com>
2021-02-23 19:12:42 +0000

    working on creating booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,40 @@
+class BookingsController < ApplicationController
+  before_action :set_bench, only: %i[new create]
+  before_action :set_user, only: %i[new create]
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+  
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to bench_path(@bench)
+    else
+      redirect_to new_bench_booking_path(@bench)
+    end
+
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def set_bench
+    @bench = Bench.find(params[:bench_id])
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date, :bench_id, :user_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # get 'bookings/show'
+  # get 'bookings/new'
+  # get 'bookings/create'
+  # get 'bookings/update'
+  # get 'bookings/delete'
   devise_for :users
   root to: 'pages#home'
-	resources :benches
+	resources :benches do
+    resources :bookings, only: %i[new create]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
