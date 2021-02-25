18602d9edc487f016908436dd050c8108bdef4f6
Fabio Freire <fabitwink@hotmail.com>
2021-02-25 17:35:14 +0000

    idk why i need to do this

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_02_25_163532) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_benches_on_user_id"
   end
 
