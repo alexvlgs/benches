5506e21a68e2bb2b1598930cd75d361f8fdc1130
Fabio Freire <fabitwink@hotmail.com>
2021-02-25 12:42:58 +0000

    about to start messing up the models with geocoding

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -4,4 +4,6 @@ class Bench < ApplicationRecord
   has_many :reviews
   has_many :booked_users, through: :bookings, source: :user
   has_many :reviewed_benches, through: :reviews, source: :user
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_address?
 end
